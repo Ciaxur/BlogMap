7844be9f260b702b15ab2f295e5d4cd03c09d455
Ciaxur <oomar@umich.edu>
2021-05-28 12:11:08 -0400

    📦 Move Mongoose to Backend

--- src/Database/Paper.model.ts
+++ src/Database/Paper.model.ts
@@ -1,6 +1,4 @@
-import { Schema, model } from 'mongoose';
 import { IAuthor } from './Author.model';
-
 
 const PaperType = [ 'Block', 'Article', 'White-Paper' ] as const;
 export type IPaperType = typeof PaperType[number];
@@ -12,14 +10,3 @@ export interface IPaper {
   createdAt:  Date,
   updatedAt:  Date,
 }
-
-const PaperSchema = new Schema<IPaper>({
-  title:  { type: String, minLength: 8, maxLength: 128, required: true, trim: true, indexed: true },
-  body:   { type: String, minLength: 1, required: true, trim: true },
-  type:   { type: String, enum: PaperType, required: true },
-  author: { type: 'ObjectId', ref: 'Author', required: true },
-}, {
-  timestamps: true,
-});
-
-export const PaperModel = model('Paper', PaperSchema);
--- src/Database/index.ts
+++ src/Database/index.ts
@@ -1,8 +1,16 @@
-import { AuthorModel, IAuthor } from './Author.model';
-import { IPaper, IPaperType, PaperModel } from './Paper.model';
+import { IAuthor } from './Author.model';
+import { IPaper } from './Paper.model';
+
+/** DATABASE INFUSED INTERFACES */
+interface DatabaseOptions {
+  _id:        string,
+  createdAt:  Date,
+  updatedAt:  Date,
+}
+interface IAuthorDb extends DatabaseOptions, IAuthor {}
+interface IPaperDb extends DatabaseOptions, IPaper {}
 
 // Source of Truth, if import needs to be a General Database
-export {
-  AuthorModel, IAuthor,
-  IPaper, IPaperType, PaperModel,
-};
+export type { IPaper, IPaperType } from './Paper.model';
+export type { IAuthor } from './Author.model';
+export type { IAuthorDb, IPaperDb };
