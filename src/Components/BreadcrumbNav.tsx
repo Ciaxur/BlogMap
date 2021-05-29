b73e941a61a76fd61ae814f766a8450c709029f3
Ciaxur <oomar@umich.edu>
2021-05-29 16:57:06 -0400

    📦 Add Breakcrumb nav component & update style

---
+++ src/Components/BreadcrumbNav.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { 
+  makeStyles, Breadcrumbs,
+} from '@material-ui/core';
+import { 
+  HomeOutlined as HomeIcon,
+  PagesOutlined as PagesIcon,
+} from '@material-ui/icons';
+
+const useStyles = makeStyles(theme => ({
+  link: {
+    display: 'flex',
+    textAlign: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    marginRight: theme.spacing(0.5),
+    width: 20,
+    height: 20,
+  },
+}));
+
+export interface BreadcumbLink {
+  name:     string,
+  path:     string,
+  onPress:  () => void,
+}
+
+interface Props {
+  links: BreadcumbLink[],
+}
+
+export function RenderBreadcrumbs(props: Props): JSX.Element {
+  const styles = useStyles();
+  const { links } = props;
+  
+  return(
+    <Breadcrumbs style={{
+      display: 'flex',
+      justifyContent: 'center',
+    }}>
+      <Link to='/' className={styles.link}>
+        <HomeIcon className={styles.icon} />
+        Home
+      </Link>
+      {links.map(link => (
+        <Link to={link.path} className={styles.link}>
+          <PagesIcon className={styles.icon} />
+          {link.name}
+        </Link>
+      ))}
+    </Breadcrumbs>
+  );
+}
