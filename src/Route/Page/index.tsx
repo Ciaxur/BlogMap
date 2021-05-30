95c966f07c453114c61df8bc965b7c16ec875cf5
Ciaxur <oomar@umich.edu>
2021-05-30 17:21:58 -0400

    ✨ Optimize Page Route & Add Metadata

--- src/Route/Page/index.tsx
+++ src/Route/Page/index.tsx
@@ -4,17 +4,29 @@ import { BreadcumbLink } from '../../Components/BreadcrumbNav';
 import { RootContext } from '../../Store/RootStore';
 import ReactMarkdown from 'react-markdown';
 import { IAuthorDb, IPaperDb } from '../../Database';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
     textAlign: 'left',
+    '& hr': {
+      opacity: 0.2,
+    },
+    '& h1, h2, h3, h4, h5, h6': {
+      margin: '8px 0px',
+    },
+  },
+  metadata: {
+    display: 'flex',
+    justifyContent: 'flex-end',
   },
 });
 
 // Overrides Author to be of type IAuthor
-interface IPaperList extends Omit<IPaperDb, 'author'> {
-  author: IAuthorDb,
+interface IPaperList extends Omit<IPaperDb, 'author' | 'createdAt' | 'updatedAt'> {
+  author:     IAuthorDb,
+  createdAt:  string,
+  updatedAt:  string,
 }
 
 interface Props {
@@ -36,10 +48,17 @@ export default function Page(props: Props): JSX.Element {
   const { location } = props.RouterProps;
 
   // MEMOIZED PAGE
-  const paper: IPaperList = React.useMemo(() => ({
-    ...(papers[id] as IPaperDb),
-    author: (authors[papers[id].author] as IAuthorDb),
-  }), [ papers, authors ]);
+  const paper: IPaperList = React.useMemo(() => {
+    const createdAt = new Date((papers[id] as IPaperDb).createdAt);
+    const updatedAt = new Date((papers[id] as IPaperDb).updatedAt);
+
+    return {
+      ...(papers[id] as IPaperDb),
+      author: (authors[papers[id].author] as IAuthorDb),
+      createdAt: `${createdAt.toLocaleDateString()} - ${createdAt.toLocaleTimeString()}`,
+      updatedAt: `${updatedAt.toLocaleDateString()} - ${updatedAt.toLocaleTimeString()}`,
+    };
+  }, [ papers, authors ]);
 
   // UPDATE ID BREADCRUMB
   React.useEffect(() => {
@@ -54,6 +73,10 @@ export default function Page(props: Props): JSX.Element {
   
   return (
     <div className={styles.root}>
+      <Typography className={styles.metadata} variant='caption'>
+        <em>by {paper.author.name} on {paper.createdAt}</em>
+      </Typography>
+      
       <ReactMarkdown skipHtml rawSourcePos>
         {paper.body}
       </ReactMarkdown>
